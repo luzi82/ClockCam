507e538fb83ffd18c8d89f1676b4a905035a28f0
luzi82 <luzi82@gmail.com>
2011-12-07 20:01:30 +0800

    add About page add i18n

---
+++ build.mk
@@ -0,0 +1,17 @@
+.PHONY : all clean
+
+TOOL_PATH = tool
+TOOL_I18N_PATH = ${TOOL_PATH}/i18n
+
+all : .i18n_timestamp
+
+.i18n_timestamp : src2/i18n/i18n.ods
+	java \
+		-jar ${TOOL_I18N_PATH}/CodeTemplate.jar \
+		-g ${TOOL_I18N_PATH}/code_template_global_config.xml \
+		-t ${TOOL_I18N_PATH}/ods2xml.xml \
+		src2/i18n/i18n.ods strings_auto
+	touch .i18n_timestamp
+
+clean :
+	rm .i18n_timestamp
--- src/com/luzi82/clockcam/ClockCamActivity.java
+++ src/com/luzi82/clockcam/ClockCamActivity.java
@@ -4,6 +4,9 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -40,6 +43,26 @@ public class ClockCamActivity extends Activity {
 	protected void onDestroy() {
 		super.onDestroy();
 
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.main_default, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.menu_item_about: {
+			Intent intent = new Intent(this, AboutActivity.class);
+			startActivity(intent);
+			return true;
+		}
+		default:
+			return super.onOptionsItemSelected(item);
+		}
 	}
 
 	// boolean mIsBound = false;
