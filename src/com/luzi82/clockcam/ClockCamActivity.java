869222ede793c92987f4e634ae81104b5c3983ec
luzi82 <luzi82@gmail.com>
2011-12-19 22:43:11 +0800

    Timer period option size option white balance option NTP option

--- src/com/luzi82/clockcam/ClockCamActivity.java
+++ src/com/luzi82/clockcam/ClockCamActivity.java
@@ -1,18 +1,21 @@
 package com.luzi82.clockcam;
 
 import java.io.IOException;
+import java.util.LinkedList;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.hardware.Camera;
 import android.os.Bundle;
+import android.preference.ListPreference;
 import android.preference.PreferenceActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
+
+import com.luzi82.clockcam.CameraInfo.Size;
 
 public class ClockCamActivity extends PreferenceActivity {
 
@@ -27,31 +30,30 @@ public class ClockCamActivity extends PreferenceActivity {
 		super.onCreate(savedInstanceState);
 
 		// Load the preferences from an XML resource
-		getPreferenceManager().setSharedPreferencesName(
-				PREFERENCE_NAME);
+		getPreferenceManager().setSharedPreferencesName(PREFERENCE_NAME);
 		initValue(getPreferenceManager().getSharedPreferences());
 		addPreferencesFromResource(R.xml.preferences);
-//		getPreferenceManager().getSharedPreferences()
-//				.registerOnSharedPreferenceChangeListener(this);
-		
-//		// UI
-//		setContentView(R.layout.main);
-//
-//		Button startButton = (Button) findViewById(R.id.startButton);
-//		startButton.setOnClickListener(new View.OnClickListener() {
-//			@Override
-//			public void onClick(View v) {
-//				serviceStart(v);
-//			}
-//		});
-//
-//		Button stopButton = (Button) findViewById(R.id.stopButton);
-//		stopButton.setOnClickListener(new View.OnClickListener() {
-//			@Override
-//			public void onClick(View v) {
-//				serviceStop(v);
-//			}
-//		});
+		// getPreferenceManager().getSharedPreferences()
+		// .registerOnSharedPreferenceChangeListener(this);
+
+		// // UI
+		// setContentView(R.layout.main);
+		//
+		// Button startButton = (Button) findViewById(R.id.startButton);
+		// startButton.setOnClickListener(new View.OnClickListener() {
+		// @Override
+		// public void onClick(View v) {
+		// serviceStart(v);
+		// }
+		// });
+		//
+		// Button stopButton = (Button) findViewById(R.id.stopButton);
+		// stopButton.setOnClickListener(new View.OnClickListener() {
+		// @Override
+		// public void onClick(View v) {
+		// serviceStop(v);
+		// }
+		// });
 
 		// init mCameraInfo
 		if (mCameraInfo == null) {
@@ -70,6 +72,26 @@ public class ClockCamActivity extends PreferenceActivity {
 			} else {
 				throw new Error("cannot get camera info");
 			}
+		}
+
+		// size
+		LinkedList<String> entries = new LinkedList<String>();
+		LinkedList<String> entryValues = new LinkedList<String>();
+		for (Size s : mCameraInfo.mSupportedPictureSizes) {
+			entries.add(String.format("%d × %d", s.mWidth, s.mHeight));
+			entryValues.add(String.format("%dx%d", s.mWidth, s.mHeight));
+		}
+		ListPreference lp = (ListPreference) findPreference("preference_setting_photo_size");
+		lp.setEntries(entries.toArray(new String[0]));
+		lp.setEntryValues(entryValues.toArray(new String[0]));
+
+		lp = (ListPreference) findPreference("preference_setting_photo_whitebalance");
+		if ((mCameraInfo.mSupportedWhiteBalance != null) && (mCameraInfo.mSupportedWhiteBalance.length > 1)) {
+			lp.setEntries(mCameraInfo.mSupportedWhiteBalance);
+			lp.setEntryValues(mCameraInfo.mSupportedWhiteBalance);
+		}else{
+			lp.setEnabled(false);
+			lp.setShouldDisableView(true);
 		}
 
 		startService(new Intent(this, ClockCamService.class));
